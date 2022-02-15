532433265a53553bcb166aa3477f28ea71489be0
zzeekk <zach.kull@gmail.com>
2022-02-15 21:45:35 +0100

    add Json4s compatibility function getStrictSerializationFormat

--- src/main/scala/org/apache/spark/util/Json4sCompat.scala
+++ src/main/scala/org/apache/spark/util/Json4sCompat.scala
@@ -1,18 +1,27 @@
 package org.apache.spark.util
 
-import org.json4s.{CustomSerializer, Formats, JValue}
+import org.json4s.jackson.Serialization
+import org.json4s.{CustomSerializer, Formats, JValue, ShortTypeHints}
+
 import scala.reflect.ClassTag
 
 /**
  * This class implements functions to use different Spark/Json4s versions with the same interface
+ * Spark 3.1 uses Json4s 3.7-M5
+ * Spark 3.2 uses Json4s 3.7-M11
  */
 object Json4sCompat {
   /**
    * Json4s up to 3.7-M5 needs Manifest, later versions need ClassTag
-   * Spark 3.1 uses Json4s 3.7-M5
-   * Spark 3.2 uses Json4s 3.7-M11
    */
   def getCustomSerializer[A: ClassTag: Manifest](ser: Formats => (PartialFunction[JValue, A], PartialFunction[Any, JValue])): CustomSerializer[A] = {
     new CustomSerializer[A](ser)
+  }
+
+  /**
+   * Json4s formats.withStrictMapExtraction does not yet exists in 3.7-M5
+   */
+  def getStrictSerializationFormat(typeHints: ShortTypeHints) = {
+    Serialization.formats(typeHints).withStrictArrayExtraction.withStrictOptionParsing
   }
 }
