cee8ffefb487680cee4bee84d6a6c69168ad2898
zzeekk <zach.kull@gmail.com>
2022-02-16 16:53:43 +0100

    add Json4s compatibility function getCustomKeySerializer

--- src/main/scala/org/apache/spark/util/Json4sCompat.scala
+++ src/main/scala/org/apache/spark/util/Json4sCompat.scala
@@ -1,7 +1,7 @@
 package org.apache.spark.util
 
 import org.json4s.jackson.Serialization
-import org.json4s.{CustomSerializer, Formats, JValue, ShortTypeHints}
+import org.json4s.{CustomKeySerializer, CustomSerializer, Formats, JValue, ShortTypeHints}
 
 import scala.reflect.ClassTag
 
@@ -11,6 +11,7 @@ import scala.reflect.ClassTag
  * Spark 3.2 uses Json4s 3.7-M11
  */
 object Json4sCompat {
+
   /**
    * Json4s up to 3.7-M5 needs Manifest, later versions need ClassTag
    */
@@ -19,9 +20,16 @@ object Json4sCompat {
   }
 
   /**
+   * Json4s up to 3.7-M5 needs Manifest, later versions need ClassTag
+   */
+  def getCustomKeySerializer[A: ClassTag: Manifest](ser: Formats => (PartialFunction[String, A], PartialFunction[Any, String])): CustomKeySerializer[A] = {
+    new CustomKeySerializer[A](ser)
+  }
+
+  /**
    * Json4s formats.withStrictMapExtraction does not yet exists in 3.7-M5
    */
-  def getStrictSerializationFormat(typeHints: ShortTypeHints) = {
+  def getStrictSerializationFormat(typeHints: ShortTypeHints): Formats = {
     Serialization.formats(typeHints).withStrictArrayExtraction.withStrictOptionParsing
   }
 }
