a1a5ae0e30ad3e005a1f1d20cadf3e4a8ef52c27
Kull Zacharias <zacharias.kull@elca.ch>
2024-02-13 13:04:41 +0100

    fix LazyListMapWrapper for Scala 2.13
    (cherry picked from commit 8ec00f1e8fe0bca5671e2e6e7f08306d91f33cae)

--- src/main/scala/org/apache/spark/util/LazyListMapWrapper.scala
+++ src/main/scala/org/apache/spark/util/LazyListMapWrapper.scala
@@ -1,6 +1,6 @@
 package org.apache.spark.util
 
-import scala.collection
+import scala.collection.{Map, AbstractMap}
 import scala.collection.immutable.ListMap
 
 /**
@@ -12,12 +12,12 @@ import scala.collection.immutable.ListMap
  * Note that its difficult to find an implementation that compiles for Scala 2.12 and 2.13.
  * It's possible with collection.AbstractMap, but not collection.immutable.AbstractMap...
  */
-class LazyListMapWrapper[A,B](createFn: () => ListMap[A,B]) extends collection.AbstractMap[A,B] with Serializable {
+class LazyListMapWrapper[A,B](createFn: () => ListMap[A,B]) extends AbstractMap[A,B] with Serializable {
   private lazy val wrappedList: ListMap[A,B] = createFn()
   override def size: Int = wrappedList.size
   def get(key: A): Option[B] = wrappedList.get(key) // removed in 2.9: orElse Some(default(key))
   def iterator: Iterator[(A, B)] = wrappedList.iterator
   override def -(key: A): Map[A, B] = wrappedList - key
   override def -(key1: A, key2: A, keys: A*): Map[A, B] = wrappedList -(key1,key2,keys:_*)
-  override def +[V1 >: B](kv: (A, V1)): collection.Map[A, V1] = wrappedList + kv
+  override def +[V1 >: B](kv: (A, V1)): Map[A, V1] = wrappedList + kv
 }
